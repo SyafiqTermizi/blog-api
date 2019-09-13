af27010e3c4bd3f34ff56b00e89ed8bcd0320855
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-09-13 22:06:00 +0800

    add pagination for post

--- config/settings.py
+++ config/settings.py
@@ -124,8 +124,7 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.User'
 
 CORS_ORIGIN_WHITELIST = [
-    "http://127.0.0.1:3000",
-    "http://localhost:3000"
+    'http://localhost:8100'
 ]
 
 REST_FRAMEWORK = {
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Post
 from .serializers import PostSerializer
@@ -8,6 +9,7 @@ from .serializers import PostSerializer
 class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
         owner_actions = ['update', 'partial_update', 'destroy']
